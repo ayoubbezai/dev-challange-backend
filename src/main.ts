4405187ef27e2a579ce35717659d12a1e1b6c8d2
ayoubbezai <bezaiayoub8@gmail.com>
2025-12-10 04:56:45 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
