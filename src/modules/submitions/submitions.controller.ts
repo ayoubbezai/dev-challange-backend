8c4760501a007e947fdf3d7ccfcfce32add6c649
ayoubbezai <bezaiayoub8@gmail.com>
2025-12-12 01:55:01 +0100

    protect routes

--- src/modules/submitions/submitions.controller.ts
+++ src/modules/submitions/submitions.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Post, Body, UseGuards  ,Get} from '@nestjs/common';
+import { Controller, Post, Body, UseGuards  ,Get } from '@nestjs/common';
 import { SubmitionsService } from './submitions.service';
 import { AddSubmissionDto } from './dto/add-submition.dto';
 import { JwtCookieGuard } from '../auth/jwt-cookie.guard';
 import { User } from '../../common/decorators/user.decorator';
 
+
+import { Roles } from '../auth/roles.decorator';
+
+import {  RolesGuard } from 'src/modules/auth/roles.guard';
 @Controller('submissions')
 export class SubmitionsController {
   constructor(private readonly submitionsService: SubmitionsService) {}
@@ -14,7 +18,8 @@ export class SubmitionsController {
     return this.submitionsService.addSubmission(dto, user.sub);
   }
 
-  // @UseGuards(JwtCookieGuard)
+  @UseGuards(JwtCookieGuard , RolesGuard)
+  @Roles('admin')
   @Get()
   async getSubmission() {
     return this.submitionsService.getSubmitions();
